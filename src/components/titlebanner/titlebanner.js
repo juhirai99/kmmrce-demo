9c2c8f62a61d904b7a6485629f8e9bca20402149
juhirai99 <juhirai99@gmail.com>
2021-08-28 17:15:59 +0400

    code updated

--- src/components/titlebanner/titlebanner.js
+++ src/components/titlebanner/titlebanner.js
@@ -1,20 +1,33 @@
 import React, { useCallback, useState } from "react";
 import * as Styled from "./titlebanner.styles";
 import { Wrapper } from "../wrapper/wrapper.styles";
-import {MobileMenu} from "../mobile-menu/mobile-menu"
 import {Button} from "../button/button"
 import useBreakpoint from "../../hooks/useBreakpoint";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MENU_LIST from "../../service/navigation.mock";
+import MENU_MOCK from "../../service/navigation.mock"
 
 export const Titlebanner = () => {
-  const [isOpen, setIsModalOpen] = useState(false);
+  const [isOpen, openModal] = useState(false);
   const { isLaptop } = useBreakpoint();
   const toggleNavigationBar = useCallback(() => {
-    setIsModalOpen(!isOpen);
+    openModal(!isOpen);
   }, [isOpen]);
+  const menuVisibility = {
+    show: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: "100%" },
+  };
   
+  const navigation = {
+    show: { opacity: 1},
+    hidden: { opacity: 0 }
+  };
+  
+  const navVisibility = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
   return (
     <Styled.Banner>
       <Wrapper>
@@ -29,19 +42,25 @@ export const Titlebanner = () => {
           {isLaptop &&
           <Styled.Navigation>
           <Styled.List>
-            {MENU_LIST.menu.map(({ name, link }) => (
-                <a href={`${link}`} key={link}><Styled.Item>{name}</Styled.Item></a>
-            ))}
+            {MENU_LIST.menu.map(({ name, link }) => (<a href={`${link}`} key={link}><Styled.Item>{name}</Styled.Item></a>))}
           </Styled.List>
         </Styled.Navigation>
-          
           }
           {isLaptop && <Button type="lightColor">Request Demo</Button>}
         </Styled.BannerWrapper>
         <Styled.NavigationDrawer>
-          {isOpen && <MobileMenu isOpen={isOpen} />}
-        </Styled.NavigationDrawer>
-        
+          {isOpen &&
+          <Styled.MobileMenu variants={menuVisibility} initial={{ x: "100%" }}
+          animate={isOpen ? "show" : "hidden"} transition={{ duration: 0.5 }}
+          >
+          <Styled.Menu variants={navigation} initial="hidden" animate="show">
+         {MENU_MOCK.menu.map(({ name }) => (
+         <Styled.Nav variants={navVisibility} key={name}>{name}</Styled.Nav>
+         ))}
+        </Styled.Menu>
+        </Styled.MobileMenu>
+          }
+        </Styled.NavigationDrawer> 
       </Wrapper>
    </Styled.Banner>
   );
